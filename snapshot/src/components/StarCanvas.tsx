98e341201df70ac345b4fcfe946aa21568f6148c
doucx <doucxldh@gmail.com>
2026-08-20 16:48:09 +0800

    biome Capture: M StarCanvas.tsx, M AnswerDiagnosticBar.tsx, M Choice2AfcContainer.tsx ... and 4 more files
    X-Quipu-Output-Tree: de24f9c637faa5ea69e0918f6981402d791d553a

--- snapshot/src/components/StarCanvas.tsx
+++ snapshot/src/components/StarCanvas.tsx
@@ -1,7 +1,12 @@
 import { useEffect, useRef, useState } from 'preact/hooks';
 import type { HitResult, Point, QuestionData } from '../types';
 import { drawDot, renderInteractivePointGrid } from '../utils/canvas/drawPointGrid';
-import { CANVAS_SIZE, checkHit, findNearestGridPoint, getDynamicDotRadius } from '../utils/geometry';
+import {
+  CANVAS_SIZE,
+  checkHit,
+  findNearestGridPoint,
+  getDynamicDotRadius,
+} from '../utils/geometry';
 
 interface StarCanvasProps {
   question: QuestionData;
